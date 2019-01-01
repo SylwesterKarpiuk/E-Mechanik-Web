db32478a26c425851acc832c3c7656061f004cf5
Sylwester Karpiuk <sylwesterkarpiuk@gmail.com>
2019-01-01 10:26:11 +0100

    Dodaj pliki projektów.

---
+++ E-Mechanik Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Mechanik_Web.Startup))]
+namespace E_Mechanik_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
