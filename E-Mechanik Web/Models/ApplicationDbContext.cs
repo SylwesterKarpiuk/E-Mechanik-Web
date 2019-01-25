e2e8b0582366f7143289b7631ac93b855e50f540
Sylwester Karpiuk <sylwesterkarpiuk@gmail.com>
2019-01-25 23:05:40 +0100

    - Dodanie wstępnego modelu profilu użytkownika, oraz aktualizacja bazy danych

--- E-Mechanik Web/Models/ApplicationDbContext.cs
+++ E-Mechanik Web/Models/ApplicationDbContext.cs
@@ -27,6 +27,7 @@ namespace E_Mechanik_Web.Models
         public System.Data.Entity.DbSet<E_Mechanik_Web.Models.AvailableService> AvailableServices { get; set; }
         public System.Data.Entity.DbSet<E_Mechanik_Web.Models.AvailableServiceCategories> AvailableServiceCategories { get; set; }
         public System.Data.Entity.DbSet<E_Mechanik_Web.Models.MechanicProfiles> MechanicProfiles { get; set; }
+        public System.Data.Entity.DbSet<E_Mechanik_Web.Models.ClientProfile> ClientProfiles { get; set; }
     }
 
 }
